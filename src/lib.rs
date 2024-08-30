6aa21bf3fa07bb022b13d9d7218d6219b6703e4d
Rudolf Zeidler <rudi@rzeidler.eu>
2024-08-30 10:46:05 +0200

    Update comments

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,17 @@ use std::num::{NonZeroUsize,NonZeroU32};
 pub fn gen_pwd(entropy: NonZeroU32, block_size: NonZeroUsize, block_sep: char) -> String {
     let alphabet: Vec<char> = ('a'..='z').chain('0'..='9').collect();
     let len_alphabet = alphabet.len();
-    // println!("{}", String::from_iter(&alphabet));
-    assert!(len_alphabet > 0, "Alphabet must be non-empty.");
-
     let alphabet_dist = Slice::new(&alphabet).unwrap();
 
     // Calculate number of blocks needed to achieve desired entropy.
     let bits_per_block = (block_size.get() as f64) * (len_alphabet as f64).log2();
     let n_blocks = (entropy.get() as f64 / bits_per_block).ceil() as usize;
 
+    // Generate password blocks
     let blocks: Vec<String> = (0..n_blocks)
         .map(|_| OsRng.sample_iter(&alphabet_dist).take(block_size.get()).collect())
         .collect();
+    
+    // Join the blocks and return password
     blocks.join(&block_sep.to_string())
 }
